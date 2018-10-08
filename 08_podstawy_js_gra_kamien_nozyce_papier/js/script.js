55967dc270c78b68b53245402ab50e978175ba0a
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2018-10-08 17:53:37 +0200

    Final version fo the game

--- 08_podstawy_js_gra_kamien_nozyce_papier/js/script.js
+++ 08_podstawy_js_gra_kamien_nozyce_papier/js/script.js
@@ -1,19 +1,24 @@
 var btnScissors = document.querySelector('#btn-scissors');
 var btnRock = document.querySelector('#btn-rock');
 var btnPaper = document.querySelector('#btn-paper');
+var btnNewGame = document.querySelector('#btn-newgame');
 
 var outputText = document.getElementById('output');
 var outputResult = document.getElementById('result');
 
 var resultPlayer = 0;
 var resultComputer = 0;
+var gamesPlayed = 0;
+var gamesLimit = 5;
+
+var gameOn = true;
 
 var computerMove = function () {
     var move = (Math.ceil(Math.random()*3));
     if (move === 1) {
         return 'paper';
     }
-    else if (move ===2) {
+    else if (move === 2) {
         return 'rock';
     }
     else return 'scissors';
@@ -22,6 +27,10 @@ var computerMove = function () {
 var writeResultToOutput = function (result, playerSymbol, computerSymbol) {
     outputText.insertAdjacentHTML('afterbegin', (result + ': you played ' + playerSymbol.toUpperCase() + ', computer played ' + computerSymbol.toUpperCase() + '<br>'));
 };
+
+var writeToOutput = function (text) {
+    outputText.insertAdjacentHTML('afterbegin', text);
+}
 
 var getResult = function (playerSymbol, computerSymbol) {
     if (playerSymbol === computerSymbol) {
@@ -46,22 +55,61 @@ var increaseCounters = function (result) {
 };
 
 var playerMove = function(event) {
-    var playerSymbol = event.currentTarget.symbol;
-    var computerSymbol = computerMove();
-    var result = getResult(playerSymbol, computerSymbol);
-    console.log(playerSymbol);
-    console.log(computerSymbol);
-    console.log(result);
-    writeResultToOutput(result, playerSymbol, computerSymbol);
-    increaseCounters(result);
+    if (gameOn === true) {
+        var playerSymbol = event.currentTarget.symbol;
+        var computerSymbol = computerMove();
+        var result = getResult(playerSymbol, computerSymbol);
+        console.log(playerSymbol);
+        console.log(computerSymbol);
+        console.log(result);
+        writeResultToOutput(result, playerSymbol, computerSymbol);
+        increaseCounters(result);
+        if (resultPlayer === gamesLimit) {
+            writeToOutput("YOU WON THE ENTIRE GAME!!!<br>");
+            gameOn = false;
+        }
+        else if (resultComputer === gamesLimit) {
+            writeToOutput("YOU LOST THE ENTIRE GAME!!!<br>");
+            gameOn = false;
+        };
+    }
+    else {
+        writeToOutput('GAME OVER, press New Game button to begin again<br>');
+    };
+};
 
+var convertToInteger = function (text) {
+    inputNumber = parseInt(text);
+    if (typeof(inputNumber) === 'number' && !isNaN(inputNumber)) {
+        return inputNumber;
+    }
+    else {
+        alert('Wrong value! Not a numer mate.')
+    }
+};
+
+var beginNewGame = function(event) {
+    gamesLimit = convertToInteger(window.prompt('How many rounds to play?'));
+    if (gamesLimit !== 'undefined') {
+        gameOn = true;
+        gamesPlayed = 0;
+        resultPlayer = 0;
+        resultComputer = 0;
+        outputText.innerHTML = '';
+        outputResult.innerHTML = 'P:C --> 0:0';
+        document.getElementById('winningResult').innerHTML = "Winning rounds required: " + gamesLimit;
+    };
+    console.log('games limit:' + gamesLimit);
 };
 
 btnScissors.addEventListener('click', playerMove);
 btnRock.addEventListener('click', playerMove);
 btnPaper.addEventListener('click', playerMove);
+btnNewGame.addEventListener('click', beginNewGame);
 
 btnScissors.symbol = 'scissors';
 btnRock.symbol = 'rock';
 btnPaper.symbol = 'paper';
 
+outputResult.innerHTML = 'P:C --> 0:0';
+document.getElementById('winningResult').innerHTML = "<br>Winning rounds required: " + gamesLimit;

7220b454aa6eb7658493ddbbe391354c1b77f24b
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2018-11-29 16:15:27 +0100

    Create and initialize carousel

--- 14_pluginy_js/js/script.js
+++ 14_pluginy_js/js/script.js
@@ -1,4 +1,15 @@
 'use strict';
 (function(){
+    var elem = document.querySelector('.carousel');
+
+    // initialize flickity carousel
+    var flkty = new Flickity( elem, {
+        // options
+        imagesLoaded: true,
+        pageDots: false,
+        percentPosition: false,
+        hash: true
+    });
+
 
 })();

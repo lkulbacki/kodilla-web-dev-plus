912421f924874c1ceec3266a904791cfe5a6f187
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2018-11-30 18:22:12 +0100

    Create Phone constructor with first prototype

--- 15_oojs/js/script.js
+++ 15_oojs/js/script.js
@@ -1,4 +1,22 @@
 'use strict';
-(function(window){
+(function(){
+    var Phone = function (brand, price, color, make) {
+        this.brand = brand;
+        this.price = price;
+        this.color = color;
+        this.make = make;
+    };
 
-})(window);
+    Phone.prototype.printInfo = function() {
+        console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ". Made in " + this.make + ".");
+    };
+
+    var phone_01 = new Phone('Samsung Galaxy S6', 2000.00, 'Darkblue', 'Korea');
+    var phone_02 = new Phone('iPhone 6s', 4000.00, 'Goldrush', 'China');
+    var phone_03 = new Phone('OnePlus One', 1000.00, 'White', 'Taiwan');
+
+    phone_01.printInfo();
+    phone_02.printInfo();
+    phone_03.printInfo();
+
+})();

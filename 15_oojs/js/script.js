0a3b6cd80042d785de37d59137c46d59ff9d8a84
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2018-11-30 19:14:09 +0100

    Add buttons with regard to 'this' context

--- 15_oojs/js/script.js
+++ 15_oojs/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 (function(){
+    // play with classes and constructors
     var Phone = function (brand, price, color, make) {
         this.brand = brand;
         this.price = price;
@@ -18,5 +19,30 @@
     phone_01.printInfo();
     phone_02.printInfo();
     phone_03.printInfo();
+
+    // play with context
+    function Button(text) {
+        this.text = text || 'Hello';
+    }
+
+    Button.prototype = {
+        create: function() {
+            // pass 'this' cotnext to a new variable to use it in alerting function
+            // otherwise, alerting function won't recognize the context properly
+            var self = this;
+            this.element = document.createElement('button');
+            this.element.innerText = this.text;
+            this.element.addEventListener('click', function() {
+                alert(self.text);
+            });
+            document.body.appendChild(this.element);
+        }
+    };
+
+    var btn1 = new Button('Hello!');
+    var btn2 = new Button('A drudzy będą na samym dole.');
+
+    btn1.create();
+    btn2.create();
 
 })();

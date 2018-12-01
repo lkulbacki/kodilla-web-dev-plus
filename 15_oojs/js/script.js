e5b8f8e5b4d270d82eabdec7688e7a975b624e96
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2018-12-01 13:39:07 +0100

    Create interconnection between columns with board functionality

--- 15_oojs/js/script.js
+++ 15_oojs/js/script.js
@@ -48,7 +48,18 @@
         }
     };
 
+
+    var board = {
+        name: 'Kanban Board',
+        addColumn: function(column) {
+            this.element.appendChild(column.element);
+        },
+        element: document.querySelector('#board .column-container')
+    };
+
     document.querySelector('#board .create-column').addEventListener('click', function() {
         var name = prompt('Enter a column name');
+        var column = new Column(name);
+        board.addColumn(column);
     });
 })();

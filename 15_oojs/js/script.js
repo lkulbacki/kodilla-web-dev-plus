d99b1492e0f669a7eaa075da23df111a498a2247
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2018-12-01 13:30:44 +0100

    Create board structure

--- 15_oojs/js/script.js
+++ 15_oojs/js/script.js
@@ -1,4 +1,7 @@
 'use strict';
 (function(){
 
+    document.querySelector('#board .create-column').addEventListener('click', function() {
+        var name = prompt('Enter a column name');
+    });
 })();

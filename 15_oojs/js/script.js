12884d435cc3357773c52f78a1bc522ecde9da99
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2018-12-03 00:13:20 +0100

    Add styling to the kanban board

--- 15_oojs/js/script.js
+++ 15_oojs/js/script.js
@@ -1,6 +1,8 @@
 'use strict';
 (function(){
 
+    document.addEventListener('DOMContentLoaded', function() {
+    });
 
     function randomString() {
         var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';

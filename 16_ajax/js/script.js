dabc7d1ac2deb81f3d3ecf8291c23c2029641d85
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2018-12-03 19:59:33 +0100

    Move jokes to jokes.html and add quotes section

--- 16_ajax/js/script.js
+++ 16_ajax/js/script.js
@@ -1,24 +1,45 @@
 'use strict';
 (function(){
-    var url = 'https://api.icndb.com/jokes/random';
+    var tweetLink = "https://twitter.com/intent/tweet?text=";
+    var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
-    var button = document.getElementById('get-joke');
-    button.addEventListener('click', function(){
-        getJoke();
-    });
-
-    var paragraph = document.getElementById('joke');
-
-    function getJoke() {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.addEventListener('load', function(){
-            var response = JSON.parse(xhr.response);
-            paragraph.innerHTML = response.value.joke;
-        });
-        xhr.send();
+    function getQuote() {
+        fetch(quoteUrl, { cache: "no-store" })
+            .then(function(resp) {
+                return resp.json();
+            })
+            .then(createTweet);
     }
 
-    getJoke();
-    
+    function createTweet(input) {
+        var data = input[0];
+
+        var dataElement = document.createElement('div');
+        dataElement.innerHTML = data.content;
+        var quoteText = dataElement.innerText.trim();
+        var quoteAuthor = data.title;
+
+        if (!quoteAuthor.length) {
+            quoteAuthor = "Unknown author";
+        }
+
+        var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+        if (tweetText.length > 140) {
+            getQuote();
+        } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        document.querySelector('.quote').innerText = quoteText;
+        document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+        document.querySelector('.tweet').setAttribute('href', tweet);
+    }
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        getQuote();
+        document.querySelector('.trigger').addEventListener('click', function() {
+            getQuote();
+        });
+    });
+
 })();

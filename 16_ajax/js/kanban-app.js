a996d9ddaa398f2ddffcaf13fddcbd1c8b1f8870
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2018-12-04 13:37:25 +0100

    Connect kanban board with API

--- 16_ajax/js/kanban-app.js
+++ 16_ajax/js/kanban-app.js
@@ -1,13 +1,11 @@
-// OGÓLNA FUNKCJA
-function randomString() {
-    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-    var str = '', i;
-    for (i = 0; i < 10; i++) {
-        str += chars[Math.floor(Math.random() * chars.length)];
-    }
-    return str;
-}
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+    'X-Client-Id': '3686',
+    'X-Auth-Token': 'f40c6e118a0c2e1d117741d26bd1cae2'
+};
 
+// Obsługa template'ów
 function generateTemplate(name, data, basicElement) {
     var template = document.getElementById(name).innerHTML;
     var element = document.createElement(basicElement || 'div');
@@ -18,22 +16,26 @@ function generateTemplate(name, data, basicElement) {
     return element;
 }
 
-console.log('app');
+// wczytanie danych board'u
+fetch(prefix + baseUrl + '/board', { headers: myHeaders })
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(resp) {
+        setupColumns(resp.columns);
+    });
 
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
+function setupColumns(columns) {
+    columns.forEach(function(column) {
+        var col = new Column(column.id, column.name);
+        board.addColumn(col);
+        setupCards(col, column.cards);
+    });
+}
 
-// DODAWANIE KOLUMN DO TABLICY
-board.addColumn(todoColumn);
-board.addColumn(doingColumn);
-board.addColumn(doneColumn);
-
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
-
-// DODAWANIE KART DO KOLUMN
-todoColumn.addCard(card1);
-doingColumn.addCard(card2);
+function setupCards(col, cards) {
+    cards.forEach(function (card) {
+        var cardObj = new Card(card.id, card.name);
+        col.addCard(cardObj);
+    });
+}

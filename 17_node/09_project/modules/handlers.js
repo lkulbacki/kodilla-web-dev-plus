03fbf7f1a7f42b3765462ffedb1dd017d43793b3
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2018-12-05 18:26:20 +0100

    Add original filename handling

--- 17_node/09_project/modules/handlers.js
+++ 17_node/09_project/modules/handlers.js
@@ -5,10 +5,10 @@ exports.upload = function(request, response) {
     console.log("Rozpoczynam obsługę żądania upload.");
     var form = new formidable.IncomingForm();
     form.parse(request, function(error, fields, files) {
-        fs.renameSync(files.upload.path, "test.png");
+        fs.renameSync(files.upload.path, files.upload.name);
         response.writeHead(200, {"Content-Type": "text/html"});
         response.write("received image:<br/>");
-        response.write("<img src='/show' />");
+        response.write("<img src='/show?" + files.upload.name + "' />");
         response.end();
     });
 };
@@ -23,13 +23,14 @@ exports.welcome = function(request, response) {
 };
 
 exports.error = function(request, response) {
-    console.log("Nie wiem co robić.");
+    console.log("ERROR Nie wiem co robić.".red);
     response.write("404 :(");
     response.end();
 };
 
-exports.show = function(request, response) {
-    fs.readFile("test.png", "binary", function(error, file) {
+exports.show = function(request, response, filename) {
+    console.log("INFO Wysyłam zdjęcie o nazwie: " + decodeURI(filename));
+    fs.readFile(decodeURI(filename), "binary", function(error, file) {
         response.writeHead(200, {"Content-Type": "image/png"});
         response.write(file, "binary");
         response.end();
--- 17_node/09_project/modules/server.js
+++ 17_node/09_project/modules/server.js
@@ -5,9 +5,16 @@ var handlers = require('./handlers'); // nasz moduł
 
 function start() {
     function onRequest(request, response) {
-        console.log("Odebrano zapytanie.".green);
-        console.log("Zapytanie " + request.url + " odebrane.");
-
+        console.log("INFO Odebrano zapytanie.".green);
+        console.log("INFO Zapytanie " + request.url + " odebrane.");
+        var paramSeparatorIndex = request.url.toString().indexOf("?");
+        var urlParams = null;
+        if (paramSeparatorIndex > -1) {
+            urlParams = request.url.toString().substr(paramSeparatorIndex + 1);
+            request.url = request.url.toString().substr(0, paramSeparatorIndex);
+        }
+        console.log('INFO urlParams: ' + urlParams);
+        console.log('INFO request.url : ' + request.url);
         response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
 
         switch (request.url) { // switch rozróżniający zapytania
@@ -19,7 +26,7 @@ function start() {
                 handlers.upload(request, response);
                 break;
             case '/show':
-                handlers.show(request, response);
+                handlers.show(request, response, urlParams);
                 break;
             default:
                 handlers.error(request, response);
@@ -28,7 +35,7 @@ function start() {
 
     http.createServer(onRequest).listen(9000);
 
-    console.log("Uruchomiono serwer!".green);
+    console.log("INFO Uruchomiono serwer!".green);
 }
 
 exports.start = start;

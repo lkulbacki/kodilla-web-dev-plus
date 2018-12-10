5c18743fe6677e7ba766eba707d5a5fdafbdb49f
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2018-12-10 18:08:15 +0100

    Refactor 18.4

--- 18_react/04_first_component/script.js
+++ 18_react/04_first_component/script.js
@@ -1,26 +1,30 @@
 var movies = [
     {
         id: 1,
-        key: 1,
         title: 'Harry Potter',
         desc: 'film o czarodzieju',
-        poster: {src: 'https://cdn.cinematerial.com/p/297x/qfeehcgu/harry-potter-and-the-chamber-of-secrets-movie-poster-md.jpg',
-            alt: 'Harry poster'}
+        poster: {
+            src: 'https://cdn.cinematerial.com/p/297x/qfeehcgu/harry-potter-and-the-chamber-of-secrets-movie-poster-md.jpg',
+            alt: 'Harry poster'
+        }
     },
     {
         id: 2,
-        key: 2,
         title: 'Król Lew',
         desc: 'Film o królu sawanny',
-        poster: {src: 'https://images-na.ssl-images-amazon.com/images/I/51M7QPXDNPL._SY445_.jpg',
-            alt: 'Lion King Poster'}
+        poster: {
+            src: 'https://images-na.ssl-images-amazon.com/images/I/51M7QPXDNPL._SY445_.jpg',
+            alt: 'Lion King Poster'
+        }
     },
     {
         id: 3,
-        key: 3,
         title: 'The Man from Earth',
         desc: 'Przegadany ale dobry',
-        poster: {src: 'https://images-na.ssl-images-amazon.com/images/I/71P53vTkaDL._RI_SX200_.jpg', alt: 'The Man Poster'}
+        poster: {
+            src: 'https://images-na.ssl-images-amazon.com/images/I/71P53vTkaDL._RI_SX200_.jpg',
+            alt: 'The Man Poster'
+        }
     },
 
 ];
@@ -33,7 +37,7 @@ var Movie = React.createClass({
         poster: React.PropTypes.object.isRequired
     },
 
-    render: function() {
+    render: function () {
         return React.createElement('li', {key: this.props.id},
             React.createElement(MovieTitle, {title: this.props.title}),
             React.createElement(MovieDescription, {desc: this.props.desc}),
@@ -43,23 +47,23 @@ var Movie = React.createClass({
 });
 
 var MovieTitle = React.createClass({
-   propTypes: {
-       title: React.PropTypes.string.isRequired
-   },
+    propTypes: {
+        title: React.PropTypes.string.isRequired
+    },
 
-   render: function(){
-       return React.createElement('h2', {}, this.props.title)
-   }
+    render: function () {
+        return React.createElement('h2', {}, this.props.title)
+    }
 });
 
 var MovieDescription = React.createClass({
-   propTypes: {
-       desc: React.PropTypes.string.isRequired
-   },
+    propTypes: {
+        desc: React.PropTypes.string.isRequired
+    },
 
-   render: function(){
-       return React.createElement('p', {}, this.props.desc)
-   }
+    render: function () {
+        return React.createElement('p', {}, this.props.desc)
+    }
 });
 
 var MoviesList = React.createClass({
@@ -67,11 +71,15 @@ var MoviesList = React.createClass({
         movies: React.PropTypes.array.isRequired
     },
     render: function () {
-        var moviesElements = this.props.movies.map(function(movie){
+        var moviesElements = this.props.movies.map(function (movie) {
             // ważne: tworząc element w pętli, należy mu przekazać parametr key
-            return React.createElement(Movie, movie)});
+            return React.createElement(Movie, {
+                key: movie.id, id: movie.id, title: movie.title,
+                desc: movie.desc, poster: movie.poster
+            })
+        });
         return React.createElement('div', {},
-            React.createElement('h1', {}, 'Lista filmóww'),
+            React.createElement('h1', {}, 'Lista filmów'),
             React.createElement('ul', {}, moviesElements)
         );
     }

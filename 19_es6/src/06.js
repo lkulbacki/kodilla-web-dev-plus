c9998c3be879af6d8220959291e865368f685475
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2018-12-18 17:31:24 +0100

    Add calculator with additional functions

---
+++ 19_es6/src/06.js
@@ -0,0 +1,90 @@
+class Stopwatch {
+    constructor(display, results) {
+        this.running = false;
+        this.display = display;
+        this.results = results;
+        this.reset();
+        this.print(this.times);
+    }
+
+    reset() {
+        this.times = {
+            minutes: 0,
+            seconds: 0,
+            miliseconds: 0
+        };
+        this.print(this.times);
+    }
+
+    print() {
+        this.display.innerText = this.format(this.times);
+    }
+
+    format(times) {
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+    }
+
+    start() {
+        if (!this.running) {
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
+        }
+    }
+
+    step() {
+        if (!this.running) return;
+        this.calculate();
+        this.print();
+    }
+
+    calculate() {
+        this.times.miliseconds += 1;
+        if (this.times.miliseconds >= 100) {
+            this.times.seconds += 1;
+            this.times.miliseconds = 0;
+        }
+        if (this.times.seconds >= 60) {
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
+    }
+
+    stop() {
+        this.running = false;
+        clearInterval(this.watch);
+    }
+
+    addResult(){
+        this.results.insertAdjacentHTML('beforeend', `<li>${this.format(this.times)}</li>`);
+    }
+
+    resetResults(){
+        this.results.innerHTML = "";
+    }
+}
+
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
+
+const stopwatch = new Stopwatch(
+    document.querySelector('.stopwatch'), document.querySelector('ul.results'));
+
+let startButton = document.getElementById('start');
+startButton.addEventListener('click', () => stopwatch.start());
+
+let stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', () => stopwatch.stop());
+
+let resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', () => stopwatch.reset());
+
+let addResultButton = document.getElementById('addResult');
+addResultButton.addEventListener('click', () => stopwatch.addResult());
+
+let resetResultsButton = document.getElementById('resetResults');
+resetResultsButton.addEventListener('click', () => stopwatch.resetResults());

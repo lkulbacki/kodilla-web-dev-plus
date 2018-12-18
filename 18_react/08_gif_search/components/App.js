ec293d4c8c3adbe9f62b879a0ffcf66b45b898db
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2018-12-18 14:42:25 +0100

    Refactor gif search engine to use promises

--- 18_react/08_gif_search/components/App.js
+++ 18_react/08_gif_search/components/App.js
@@ -11,34 +11,47 @@ App = React.createClass({
         };
     },
 
-    handleSearch: function (searchingText) {  // 1.
+    handleSearch: function (searchingText) {
         this.setState({
-            loading: true  // 2.
+            loading: true
         });
-        this.getGif(searchingText, function (gif) {  // 3.
-            this.setState({  // 4
-                loading: false,  // a
-                gif: gif,  // b
-                searchingText: searchingText  // c
-            });
-        }.bind(this));
+
+        this.getGif(searchingText)
+            .then(gif => {
+                this.setState({
+                    loading: false,
+                    gif: gif,
+                    searchingText: searchingText
+                });
+            })
+            .catch(error => console.log(error));
     },
 
-    getGif: function (searchingText, callback) {  // 1.
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
-        var xhr = new XMLHttpRequest();  // 3.
-        xhr.open('GET', url);
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                var data = JSON.parse(xhr.responseText).data; // 4.
-                var gif = {  // 5.
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
+    getGif: function (searchingText) {
+        return new Promise(
+            function (resolve, reject) {
+                let url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
+                const request = new XMLHttpRequest();
+                request.open('GET', url);
+                request.onload = function () {
+                    if (this.status === 200) {
+                        let data = JSON.parse(request.responseText).data; // 4.
+                        let gif = {  // 5.
+                            url: data.fixed_width_downsampled_url,
+                            sourceUrl: data.url
+                        };
+                        resolve(gif); // Sukces
+                    } else {
+                        reject(new Error(this.statusText)); // Dostaliśmy odpowiedź, ale jest to np 404
+                    }
                 };
-                callback(gif);  // 6.
-            }
-        };
-        xhr.send();
+                request.onerror = function () {
+                    reject(new Error(
+                        `XMLHttpRequest Error: ${this.statusText}`));
+                };
+
+                request.send();
+            });
     },
 
     render: function () {

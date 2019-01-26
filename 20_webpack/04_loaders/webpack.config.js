2187eb777ea5604476571dbcea8e78eb5ca8b1f8
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2019-01-26 15:40:58 +0100

    Init project structure and webpack/babel configuration

---
+++ 20_webpack/04_loaders/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+//webpack.config.js
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};

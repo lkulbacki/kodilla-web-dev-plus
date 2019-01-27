4fd0315f4663910e1c1f682965be705cb8fbe95c
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2019-01-27 16:43:02 +0100

    Add TODO list display and appropriate components

--- 20_webpack/04_loaders/src/containers/App.js
+++ 20_webpack/04_loaders/src/containers/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
 import Title from "../components/Title";
+import TodoList from "./TodoList";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: []
+            data: [{
+                id: 1,
+                text: 'clean room'
+            }, {
+                id: 2,
+                text: 'wash the dishes'
+            }, {
+                id: 3,
+                text: 'feed my cat'
+            }]
         };
     }
 
@@ -30,6 +40,7 @@ class App extends React.Component {
             <div className={style.TodoApp}>
                 Tutaj pojawią się komponenty naszej aplikacji.
                 <Title todoList={{length: this.state.data.length}}/>
+                <TodoList todoList={{data: this.state.data}}/>
             </div>
         );
     }

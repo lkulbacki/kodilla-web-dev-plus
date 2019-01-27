b8734d73bf9f3c8613e85ef8e604600a18b15e07
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2019-01-27 13:54:43 +0100

    Update webpack configuration to include environment setting

--- 20_webpack/04_loaders/webpack.config.js
+++ 20_webpack/04_loaders/webpack.config.js
@@ -1,36 +1,42 @@
 const path = require('path');
 
 //webpack.config.js
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {loader: 'style-loader'},
-                    {
-                        loader: 'css-loader',
+module.exports = (env) => {
+    const environment = env || 'production';
+
+    return {
+
+        mode: environment,
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.' + environment + '.bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
                         options: {
-                            modules: true
+                            presets: ['@babel/preset-env']
                         }
                     }
-                ]
-            }
-        ]
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        {loader: 'style-loader'},
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
     }
 };

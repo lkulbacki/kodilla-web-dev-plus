821b37448c5b34eb730d3f71e1389dc88a79ed67
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2019-03-10 00:40:53 +0100

    Logowanie OAuth Google

---
+++ 25_expressjs/config06.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '608408675693-ahrq943rg64397jbub1l7euf21f43u5c.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'RH1PynCGUhPZ0t766lRaEEXf',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
